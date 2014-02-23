d518d870cabfea792d15f99e7ffdf25a9b201bb2
David McClure <dave.j.mcclure@gmail.com>
2014-02-22 18:04:27 -0800

    Adding Google Analytics

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -51,6 +51,17 @@
 	<p>&copy; <a href="http://davemac.info" target="_blank">Dave J. McClure</a> <?php echo date("Y"); ?></p>
 </footer>
 
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-47798416-2', 'davemac.info');
+	ga('send', 'pageview');
+
+</script>
+
 <!-- Bootstrap core JavaScript. Placed at the end of the document so the page loads faster -->
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
 
